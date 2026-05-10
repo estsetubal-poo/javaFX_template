991e65650ecfffd5ba30907a84c16006d8754d90
Patricia Macedo <59732005+patriciamacedo@users.noreply.github.com>
2026-05-10 17:54:47 +0100

    Update package declaration and clean imports
    Changed package declaration from 'org.example' to 'ips.poo' and removed unused imports.

--- src/main/java/ips/poo/App.java
+++ src/main/java/ips/poo/App.java
@@ -1,13 +1,6 @@
-package org.example;
+package ips.poo;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 
